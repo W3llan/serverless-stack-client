c478670faddc565d4ed2ea49e79d6b9f42ce7af8
Romain Pinglot <r.pinglot@gmail.com>
2020-02-11 15:48:09 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-romain"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://2kcrxmk6uh.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_QPmHZCioH",
+      APP_CLIENT_ID: "1c65soqpd89h1hoc05v7tqc2tn",
+      IDENTITY_POOL_ID: "us-east-1:38f8efcb-a008-4a4d-be48-67bd46669cea"
+    }
+  };
